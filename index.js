eadc63ac36b0e276a6c4f9b4cbfdf8568d9993ea
Bhavi09 <mehtabhavi8@gmail.com>
2022-08-31 19:03:35 +0530

    date 31

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ import { db } from './config.js';
 import Axios from 'axios';
 import { initializeApp } from "firebase-admin/app";
 import pkg from 'firebase-admin';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { createRequire } from "module";
+
+
+
+
 const require = createRequire(import.meta.url);
 const credentials = require("./serviceAccount.json");
 const { credential } = pkg;
-
-
 const { auth } = pkg;
 const app = express();
 
@@ -22,9 +24,6 @@ const app = express();
 initializeApp({
     credential: credential.cert(credentials)
 });
-
-
-
 
 // Use of cors 
 app.use(cors({
@@ -36,36 +35,19 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+
 let pfo;
-//***************price function imported from app.js************************
-
-
-
-scrape_price("https://www.tickertape.in/stocks/state-bank-of-india-SBI?checklist=basic").then((response) => {
-    pfo = response;
-    app.get('/price', (_req, res) => {
-        const responseData = {
-            sbi_price: `${response}`
-        };
-        const jsonContent = JSON.stringify(responseData);
-        res.end(jsonContent);
-    })
-})
+// **********API'S***********************************************
 
 
 app.get('/', (_req, res) => {
     res.send("Server is working")
 })
 
-
-
-
-
 // ******************SIGN UP***********************************
 
-
 async function checkUserInFirebase(email) {
-    return new Promise((resolve,_reject) => {
+    return new Promise((resolve, _reject) => {
         auth().getUserByEmail(email)
             .then(() => {
                 resolve(true);
@@ -76,29 +58,28 @@ async function checkUserInFirebase(email) {
     });
 }
 
-app.get('/signup', async(req, res) => {
+app.get('/signup', async (req, res) => {
     let check = false;
     let email = req.query.email;
     let password = req.query.password;
     check = await checkUserInFirebase(email);
-    if(!check)
-    {
+    if (!check) {
 
         auth().createUser
-        (
-            {
-                email: email,
-                password: password,
-            }
-        )
-        .then(function () {
-            res.send({message:"user added"});
-            res.status(201).end();
-        })
+            (
+                {
+                    email: email,
+                    password: password,
+                }
+            )
+            .then(function () {
+                res.send({ message: "user added" });
+                res.status(201).end();
+            })
 
     }
-    else{
-        res.end({message:"User Already existed"})
+    else {
+        res.end({ message: "User Already existed" })
     }
 })
 
@@ -106,24 +87,20 @@ app.get('/signup', async(req, res) => {
 // ***************** LOGIN**********************************************
 
 
-app.get('/login',(req,res)=>{
+app.get('/login', (req, res) => {
     const auths = getAuth();
     signInWithEmailAndPassword(auths, req.query.email, req.query.password)
-      .then((userCredential) => {
-        // Signed in 
-        const _user = userCredential.user;
-        res.send({message:"User logged in"});
-        res.end();
-      })
-      .catch((_error) => {
-        res.end("User does not exist");
-      });
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            res.send({ message: "User logged in" });
+        })
+        .catch((_error) => {
+            res.send({message:"User does not exist"});
+        });
 })
 
-
-
-
-
+// ***************** SIGNOUT **********************************************
 
 
 // ***************ADD USER ON FIRESTORE************************************
@@ -150,13 +127,12 @@ app.get('/adduser', async (req, res) => {
 })
 
 
-
-// *********** ADDING USER IN FIRESTORE*************************************
+// *********** ADDING USER BUYING IN FIRESTORE*************************************
 
 
 
 app.get('/user', async (req, res) => {
-    const user = doc(db, "users", req.query.id);
+   try{ const user = doc(db, "users", req.query.id);
     let number;
     let flag1 = 0;
     let flag2 = 0;
@@ -206,8 +182,9 @@ app.get('/user', async (req, res) => {
             }],
         }, { merge: true });
     }
-    res.end();
-})
+    res.send({inres:"successfully updated"});
+}catch(error){res.send(err);}
+});
 
 // ********************** GET STATUS *************************
 
@@ -215,33 +192,32 @@ app.get('/user', async (req, res) => {
 app.get('/getstatus', async (req, res) => {
     let difference = 0;
     let total = 0;
-    let inddif=[];
+    let inddif = [];
     const docref = doc(db, "users", req.query.id);
     const docSnap = await getDoc(docref);
-    try{
-    if (docSnap.exists()) {
-        let stocksarr = docSnap.data().stock;
-        // console.log("Before for loop")
-        const promises = stocksarr.map(async (stockarr) => {
-            let value = await callfordata(stockarr.s_name);
-            console.log(value)
-            let subs = parseInt(value['data']['Global Quote']['05. price']);
-            difference = (parseInt(stockarr.invested) - subs * 79 * parseInt(stockarr.qty))
-            let obj = {"name":stockarr.s_name,"diff":difference};
-            inddif.push(obj);
-            total += difference;
-        })
-        await Promise.all(promises);
-        res.status(200).send({"P&L":total.toString(),"stocksdetail":stocksarr, "inddif":inddif});
+    try {
+        if (docSnap.exists()) {
+            let stocksarr = docSnap.data().stock;
+            // console.log("Before for loop")
+            const promises = stocksarr.map(async (stockarr) => {
+                let value = await callfordata(stockarr.s_name);
+                let subs = parseInt(value['data']['Global Quote']['05. price']);
+                difference = (subs * 79 * parseInt(stockarr.qty) - parseInt(stockarr.invested))
+                let obj = { "name": stockarr.s_name, "diff": difference };
+                inddif.push(obj);
+                total += difference;
+            })
+            await Promise.all(promises);
+            res.status(200).send({ "P&L": total.toString(), "stocksdetail": stocksarr, "inddif": inddif });
+        }
+        else {
+            res.end("No such document");
+        }
     }
-    else {
-        res.end("No such document");
+    catch (err) {
+        console.log("Error occured: " + err);
+        res.send("Error occured:" + err);
     }
-}
-catch(err){
-    console.log("Error occured: "+err);
-res.send("Error occured:"+err);
-}
 })
 
 
@@ -258,8 +234,41 @@ async function callfordata(name) {
 }
 
 
+// ************** SELL STOCK***************************************
+app.get('/sell', async (req, res) => {
+    let flag1 = 0;
+    const docref = doc(db, "users", req.query.id);
+    const docSnap = await getDoc(docref);
+    let stockarr = docSnap.data().stock;
+    let stockarr2 = [];
+    let pl = 0;
+    for (let i = 0; i < stockarr.length; i++) {
+        let temp = stockarr[i];
+        if ((req.query.name == temp["s_name"]) && (parseInt(req.query.qty) <= parseInt(temp["qty"]))) {
+            flag1 = 1;
+            let value = await callfordata(temp["s_name"]);
+            let subs = 79 * parseInt(value['data']['Global Quote']['05. price']) * parseInt(req.query.qty);
+            pl = ((79 * parseInt(value['data']['Global Quote']['05. price'])) - (parseInt(temp["invested"]) / parseInt(temp["qty"])));
+            let stock = {
+                invested: temp["invested"] - subs,
+                qty: temp["qty"] - req.query.qty,
+                s_name: temp['s_name']
+            }
+            stockarr2.push(stock);
+        }
+        else stockarr2.push(temp);
+    }
 
-
+    if (flag1 == 0) {
+        res.send({pl:"Please enter correct name or qty"});
+    }
+    else {
+        updateDoc(docref, {
+            "stock": stockarr2
+        }, { merge: true });
+        res.send({ pl: pl * parseInt(req.query.qty) });
+    }
+})
 
 // ************DELETE USER*******************************************
 
@@ -285,12 +294,19 @@ app.get('/updateuser', async (_req, res) => {
 })
 
 
-//********************* POST JUST FOR CHECKING ***********************
+//***************price function imported from app.js************************
 
-app.post('/postdata', function (req, res) {/* TODO document why this function is empty */
-    console.log(req.body);
-    res.status(201).send("Data is recieved...")
-});
+scrape_price("https://www.tickertape.in/stocks/state-bank-of-india-SBI?checklist=basic").then((response) => {
+    pfo = response;
+    app.get('/price', (_req, res) => {
+        const responseData = {
+            sbi_price: `${response}`
+        };
+        const jsonContent = JSON.stringify(responseData);
+        res.end(jsonContent);
+    })
+})
+
 
 
 app.listen(3000, (err) => {
